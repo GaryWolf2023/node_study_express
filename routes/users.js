657091a00845ccec72570a43351dbfc97232a341
husky-lby <husky._lby@petalmail.com>
2023-04-17 18:02:21 +0800

    first updata

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  console.log(req);
+  res.send('respond with a resource');
+})
+router.get('/login', function(req, res, next) {
+  console.log(req);
+  res.send('respond with a resource');
+})
+
+module.exports = router;
